3a4ac1ed4741fa4695b3ad46509b1e2290682bed
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-08-03 10:12:49 +0600

    Add modular service registration for Basket, Catalog, and Ordering modules

---
+++ src/API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Basket;
+using Catalog;
+using Ordering;
+
+namespace API.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCatalogModule(configuration);
+        services.AddBasketModule(configuration);
+        services.AddOrderingModule(configuration);
+
+        return services;
+    }
+}
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,4 +1,9 @@
+using API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
