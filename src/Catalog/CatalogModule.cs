243ab9c88309089c499fd2ca43a99422985a903a
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-08-09 21:44:54 +0600

    Add migration support and extend product image file length in Catalog module

--- src/Catalog/CatalogModule.cs
+++ src/Catalog/CatalogModule.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shared.Data;
 
 namespace Catalog;
 
@@ -12,13 +13,13 @@ public static class CatalogModule
     public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("DefaultConnection");
-        services.AddDbContext<CatalogDbContext>( options => options.UseNpgsql(connectionString));
+        services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(connectionString));
         return services;
     }
 
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app, IHostEnvironment env)
     {
-
+        app.UseMigration<CatalogDbContext>();
         return app;
     }
 
