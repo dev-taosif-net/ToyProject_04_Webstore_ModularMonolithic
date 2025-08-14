cfd31476733a6271ce5c18b017605762d2503281
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-08-14 20:26:14 +0600

    AuditableEntityInterceptor Updated

--- src/Shared/Data/Interceptors/AuditableEntityInterceptor.cs
+++ src/Shared/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -13,12 +13,13 @@ public class AuditableEntityInterceptor : SaveChangesInterceptor
         return base.SavingChanges(eventData, result);
     }
 
-    public override ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result,
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
         CancellationToken cancellationToken = new CancellationToken())
     {
         UpdateEntities(eventData.Context);
-        return base.SavedChangesAsync(eventData, result, cancellationToken);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
     }
+
 
     private void UpdateEntities(DbContext? context)
     {
