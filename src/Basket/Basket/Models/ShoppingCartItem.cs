bd21c2cbb66c1334676598a5b4cd2cc77fcba018
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-09-05 17:01:26 +0600

    Add ShoppingCart domain model and update Entity<T> class
    Updated the `Entity<T>` class to make the `Id` property non-required with a default value, addressing nullability concerns.
    
    Added `ShoppingCart` and `ShoppingCartItem` classes:
    - `ShoppingCart` includes properties for `UserName`, `Items`, and `TotalPrice`, along with methods to manage items.
    - `ShoppingCartItem` includes properties for cart item details and ensures proper initialization.
    
    Both classes follow Domain-Driven Design principles using `Entity` and `Aggregate` base classes.

---
+++ src/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,28 @@
+﻿using Shared.DDD.Entities;
+
+namespace Basket.Basket.Models;
+
+public class ShoppingCartItem : Entity<Guid>
+{
+    public Guid ShoppingCartId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; internal set; } = default!;
+    public string Color { get; private set; } = default!;
+
+    // will comes from Catalog moduleproductName
+    public decimal Price { get; private set; } = default!;
+    public string ProductName { get; private set; } = default!;
+
+    internal ShoppingCartItem( Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName) : base()  
+    {
+        ShoppingCartId = shoppingCartId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
+
+
+
+}
